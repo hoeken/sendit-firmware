a0265d0217f3786ad980f171b0ae045182e2a8d6
Zach Hoeken <hoeken@gmail.com>
2026-04-08 16:37:59 -1000

    Cleaned up libraries and tested

--- src/adchelper.h
+++ src/adchelper.h
@@ -12,8 +12,6 @@
 #include "RollingAverage.h"
 #include <ADS1X15.h>
 #include <Arduino.h>
-#include <MCP342x.h>
-#include <MCP3x6x.h>
 #include <Wire.h>
 #include <vector>
 
@@ -94,31 +92,4 @@ class ADS1115Helper : public ADCHelper
     float _vrefs[4];
     uint8_t _gains[4];
 };
-
-class MCP3425Helper : public ADCHelper
-{
-  public:
-    MCP3425Helper(MCP342x::Config& config, float vref, MCP342x* adc, uint16_t samples = RA_DEFAULT_SIZE, uint32_t window_ms = RA_DEFAULT_WINDOW);
-    void requestADCReading(uint8_t channel) override;
-    bool isADCReady() override;
-    uint32_t loadReadingFromADC(uint8_t channel) override;
-
-  private:
-    MCP342x* _adc;
-    MCP342x::Config _config;
-};
-
-class MCP3564Helper : public ADCHelper
-{
-  public:
-    MCP3564Helper(float vref, MCP3564* adc, uint16_t samples = RA_DEFAULT_SIZE, uint32_t window_ms = RA_DEFAULT_WINDOW);
-    void requestADCReading(uint8_t channel) override;
-    bool isADCReady() override;
-    uint32_t loadReadingFromADC(uint8_t channel) override;
-
-  private:
-    uint32_t _channelAddresses[8] = {MCP_CH0, MCP_CH1, MCP_CH2, MCP_CH3, MCP_CH4, MCP_CH5, MCP_CH6, MCP_CH7};
-    MCP3564* _adc;
-};
-
 #endif /* !YARR_ADC_H */
