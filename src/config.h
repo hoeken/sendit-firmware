1b28c8051b880ad3d10cd9d5c04aa76a71696a75
Zach Hoeken <hoeken@gmail.com>
2026-04-09 15:19:32 -1000

    new board urls

--- src/config.h
+++ src/config.h
@@ -30,7 +30,7 @@
 #endif
 
 #ifndef YB_PROJECT_URL
-  #define YB_PROJECT_URL "https://github.com/hoeken/sendit"
+  #define YB_PROJECT_URL "https://sendit.yarrboard.com"
 #endif
 
 #ifndef YB_GIT_URL
@@ -38,7 +38,7 @@
 #endif
 
 #ifndef YB_OTA_MANIFEST_URL
-  #define YB_OTA_MANIFEST_URL "https://hoeken.github.io/sendit-firmware/releases/ota_manifest.json";
+  #define YB_OTA_MANIFEST_URL "https://firmware.sendit.yarrboard.com/releases/ota_manifest.json";
 #endif
 
 #ifndef YB_IS_DEVELOPMENT
